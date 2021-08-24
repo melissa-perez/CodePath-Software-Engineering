e13d3dc681591f4f387ab97739ecdb863a106a13
Melissa Perez <melissa-perez@users.noreply.github.com>
2021-08-24 00:48:04 -0400

    Add files via upload

---
+++ first_unique_char.py
@@ -0,0 +1,20 @@
+def firstUniqChar(self, s: str) -> int:
+    # base case: no character exists
+    if not s:
+        return -1
+    
+    # iterate through whole string, keep count
+    char_freq = {}
+    
+    for letter in s:
+        if letter not in char_freq:
+            char_freq[letter] = 1
+        else:
+            char_freq[letter] += 1
+            
+    # go through string again to find index
+    string_length = len(s)
+    for i in range(string_length):
+        if char_freq[s[i]] == 1:
+            return i
+    return -1
