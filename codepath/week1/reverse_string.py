57dc72a6e2387c34ef025d6a81133901e5f1bd8d
Melissa Perez <melissa-perez@users.noreply.github.com>
2021-08-30 21:00:11 -0400

    Update reverse_string.py

--- codepath/week1/reverse_string.py
+++ codepath/week1/reverse_string.py
@@ -1,34 +1,34 @@
 def reverseString(self, s: List[str]) -> None:
-	"""
-	Do not return anything, modify s in-place instead.
-	"""
-	# Reverse the input list of chars in place
-	# for python we need to use a list
-	# the input is a list
-	# the output is the same list but reversed
+    """
+    Do not return anything, modify s in-place instead.
+    """
+    # Reverse the input list of chars in place
+    # for python we need to use a list
+    # the input is a list
+    # the output is the same list but reversed
 
-	# edge case: []
-	# return []
+    # edge case: []
+    # return []
 
-	# input 1: [a, b, c]
-	# output 1: [c, b, a]
+    # input 1: [a, b, c]
+    # output 1: [c, b, a]
 
-	# input 2: [a, b, c, d]
-	# output 2: [d, c, b, a]
+    # input 2: [a, b, c, d]
+    # output 2: [d, c, b, a]
 
-	# have two pointers
-	# start and end
-	# swap start and end values
-	# until you reach the middle. where the algorithm should end
+    # have two pointers
+    # start and end
+    # swap start and end values
+    # until you reach the middle. where the algorithm should end
 
-	list_of_chars_length = len(s)
-	start = 0
-	end = list_of_chars_length - 1
+    list_of_chars_length = len(s)
+    start = 0
+    end = list_of_chars_length - 1
 
-	while start < end:
-		temp = s[start]
-		s[start] = s[end]
-		s[end] = temp
+    while start < end:
+        temp = s[start]
+        s[start] = s[end]
+        s[end] = temp
 
-		start += 1
-		end -= 1
+        start += 1
+        end -= 1
