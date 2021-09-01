cb03e976dd7f02a8129c11e866a7ae70ff7f5993
Melissa Perez <melmperz@gmail.com>
2021-09-01 03:50:23 -0400

    add files

---
+++ codepath/week3/is_palindrome.py
@@ -0,0 +1,42 @@
+# Author: Melissa Perez
+# Date: --/--/--
+# Description:
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        """
+        Go through list and add it to array
+        Key will be index
+        Afterwards, do two pointer
+        one at start
+        one at end
+        go towards middle
+        if middle is reached
+        it is a palindrom
+        else not
+
+        """
+        # always check if head is valid
+        if head is None:
+            return
+
+        list_of_nodes = []
+        current_node = head
+        while current_node is not None:
+            list_of_nodes.append(current_node.val)
+            current_node = current_node.next
+
+        start = 0
+        end = len(list_of_nodes) - 1
+
+        while start < end:
+            if list_of_nodes[start] != list_of_nodes[end]:
+                return False
+            start += 1
+            end -= 1
