22c2f7b2eb35a299c4276c18b45aab551d6d7290
Melissa Perez <melmperz@gmail.com>
2021-09-01 21:04:07 -0400

    add prime_problem

---
+++ codepath/week1/next_prime.py
@@ -0,0 +1,19 @@
+# Author: Melissa Perez
+# Date: --/--/--
+# Description:
+
+def next_prime(prime):
+    next = prime + 1
+    while not is_prime(next):
+        next += 1
+    return next
+
+def is_prime(prime):
+    for i in range(2, prime - 1):
+        if prime % i == 0:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    print(next_prime(19))
