9735f20c53ae2be265e8926bd625519024fb4e69
Melissa Perez <melmperz@gmail.com>
2021-09-05 22:42:58 -0400

    Create partition_list.py

---
+++ codepath/week3/partition_list.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def printList(self, head):
+        while head is not None:
+            print(head.val, " ",end="")
+            head = head.next
+        print()
+
+            
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        less = ListNode()
+        less_runner = less
+        more = ListNode()
+        more_runner = more
+        
+        while head is not None:   
+            if head.val < x:
+                less_runner.next = head
+                less_runner = less_runner.next
+            elif head.val >= x:
+                more_runner.next = head
+                more_runner = more_runner.next         
+            head = head.next
+        more_runner.next = None
+        less_runner.next = more.next     
+        head = less.next
+        return head
+
